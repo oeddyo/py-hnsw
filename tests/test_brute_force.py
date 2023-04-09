98ee98562a6a9ec1ea63013e69b461471f64c63b
Eddie Xie <oeddyo@gmail.com>
2023-04-09 10:48:24 -0700

    WIP

--- tests/test_brute_force.py
+++ tests/test_brute_force.py
@@ -9,7 +9,7 @@ class Test(TestCase):
         bf = BruteForce(dim=10)
 
         with self.assertRaises(AssertionError):
-            bf.add("doc1", [1.0])
+            bf.add_item("doc1", [1.0])
 
     def test_bf_with_multiple_vectors(self):
         bf = BruteForce(dim=2)
@@ -18,9 +18,9 @@ class Test(TestCase):
         v2 = [1.0, 2.0]
         v3 = [100.0, 100.0]
 
-        bf.add("doc1", v1)
-        bf.add("doc2", v2)
-        bf.add("doc3", v3)
+        bf.add_item("doc1", v1)
+        bf.add_item("doc2", v2)
+        bf.add_item("doc3", v3)
 
         ans = bf.knn(1, [99, 99])
 
