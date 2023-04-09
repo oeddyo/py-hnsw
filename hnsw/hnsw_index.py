ae6449f8f1509f301f87a6fa78221386a0d46643
Eddie Xie <oeddyo@gmail.com>
2023-04-09 11:01:19 -0700

    WIP

--- hnsw/hnsw_index.py
+++ hnsw/hnsw_index.py
@@ -3,6 +3,8 @@ from typing import List, Tuple
 from common.base_index import BaseIndex
 from common.vector import Vector
 
+import math
+
 
 class HNSWIndex(BaseIndex):
 
@@ -18,18 +20,15 @@ class HNSWIndex(BaseIndex):
         # line2: ep in the paper
         self.entry_point = None
 
+        # multiplier corresponds to the "multi_" term used in hnswlib
+        # it's a multiplier derived from the parameter m and it won't change later on
+        self.multiplier = 1 / math.log(m)
+
     def add_item(self, doc_id: str, v: Vector):
         # get entry point
         ep = self.entry_point
-
-
-
-
-
 
         pass
 
     def knn(self, k: int, query_vector: Vector) -> List[Tuple[str, float]]:
         return []
-
-
