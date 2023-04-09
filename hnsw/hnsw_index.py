f1dc22b1ccae6aa95f596af035d0f3b361fd96e4
Eddie Xie <oeddyo@gmail.com>
2023-04-09 10:45:38 -0700

    WIP

--- hnsw/hnsw_index.py
+++ hnsw/hnsw_index.py
@@ -5,12 +5,31 @@ from common.vector import Vector
 
 
 class HNSWIndex(BaseIndex):
-    def __init__(self, dim=64):
+
+    def __init__(self, dim: int = 64, m: int = 16):
+        """
+        Create HNSW index
+
+        :param dim: dimension of the index.
+        :param m: parameter used by HNSW
+        """
         self.dim = dim
 
+        # line2: ep in the paper
+        self.entry_point = None
+
     def add_item(self, doc_id: str, v: Vector):
+        # get entry point
+        ep = self.entry_point
+
+
+
+
+
+
         pass
 
     def knn(self, k: int, query_vector: Vector) -> List[Tuple[str, float]]:
         return []
+
 
